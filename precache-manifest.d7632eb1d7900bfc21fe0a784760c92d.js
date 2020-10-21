c527f0cdcb9d35852db919b2312aaf6471e263b8
kareemshowno <65797037+kareemshowno@users.noreply.github.com>
2020-10-21 14:28:55 +0300

    Updates

---
+++ precache-manifest.d7632eb1d7900bfc21fe0a784760c92d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61a73dcaee2446ca703a6a93a9a8b460",
+    "url": "/barber-shop/index.html"
+  },
+  {
+    "revision": "25f0bc1717972a81ee1a",
+    "url": "/barber-shop/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "61ba60532617088d8018",
+    "url": "/barber-shop/static/css/main.a8959b2f.chunk.css"
+  },
+  {
+    "revision": "25f0bc1717972a81ee1a",
+    "url": "/barber-shop/static/js/2.988bca96.chunk.js"
+  },
+  {
+    "revision": "434eeea35bf11e3d22931f972c8fbb4c",
+    "url": "/barber-shop/static/js/2.988bca96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61ba60532617088d8018",
+    "url": "/barber-shop/static/js/main.2e27c303.chunk.js"
+  },
+  {
+    "revision": "8066bcbb1e6534ca40e0",
+    "url": "/barber-shop/static/js/runtime-main.049b443f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/barber-shop/static/media/SEGA.12a3db8b.woff"
+  }
+]);
